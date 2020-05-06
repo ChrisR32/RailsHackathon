fce7b9ffb7fd4309ad2679a99c0261378f3390c5
ChrisR32 <59920367+ChrisR32@users.noreply.github.com>
2020-05-07 08:29:00 +1000

    Added First name & Last Name text boxes to: registration signup, edit profile. ALSO: Tidyed up the look of the pages. AND changed the navbar user menu from email to the users full name.

--- wallet/app/controllers/application_controller.rb
+++ wallet/app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
         devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname])
         devise_parameter_sanitizer.permit(:sign_up, keys: [:lastname])
         devise_parameter_sanitizer.permit(:account_update, keys: [:firstname])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:lastname])        
+        devise_parameter_sanitizer.permit(:account_update, keys: [:lastname])
     end
 end
