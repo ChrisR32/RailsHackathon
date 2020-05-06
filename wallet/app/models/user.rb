8be2c8da7342b0d48168953c31bcc9d07c81d790
ChrisR32 <59920367+ChrisR32@users.noreply.github.com>
2020-05-07 06:39:41 +1000

    Added first and last name to the user table. Added validates for both to user.rb. And added permitted parameters for both to the application controller

--- wallet/app/models/user.rb
+++ wallet/app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :firstname, presence: true, length: {maximum: 50}
+  validates :lastname, presence: true, length: {maximum: 50}
+ 
 end
