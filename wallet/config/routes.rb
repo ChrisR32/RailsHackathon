eb53f3959a3aa1bf1386b22ff82cd7c84e89b926
Evelyn Paplauskas <evelyn.paplauskas@outlook.com>
2020-05-07 12:48:52 +1000

    Added routes for Expense and Occurrence models

--- wallet/config/routes.rb
+++ wallet/config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :expenses, :expense_occurrence
 end
